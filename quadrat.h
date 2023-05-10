ca928f694ed568ee48cf21a89cb4950c5f0c97bd
David@Mac <david.metzler.2003@gmail.com>
2023-05-10 16:32:43 +0200

    dav01

--- quadrat.h
+++ quadrat.h
@@ -4,9 +4,9 @@
 #define QUADRAT_H
 class QUADRAT {
   private:
-    double hoehe_schwerpunkt = 0.1; //m über dem Boden
-    double gewicht_body = 0.6;      //kg von allen Elementen außer der Scheibe
-    double gewicht_scheibe = 0.1;   //kg pro Scheibe
+    static const double hoehe_schwerpunkt; 
+    static const double gewicht_body;      
+    static const double gewicht_scheibe;   
     POSITION position;              //Position des Quadrats
     INERTIA inertia;                // Inertia als Attribut von Quadrat
     HALLSENSOR hall_sensor;         // HallSensor als Attribut von Quadrat
@@ -15,8 +15,17 @@ class QUADRAT {
     IMU_9DOF imu_9dof;              // IMU_9DOF als Attribut von Quadrat
   
   public:
-    //Konstruktor Methode
-    QUADRAT();
+    // Konstruktor Methode
+    QUADRAT(double hoehe_schwerpunkt = 0.1, double gewicht_body = 0.6, double gewicht_scheibe = 0.1, POSITION position = POSITION(), INERTIA inertia = INERTIA(), HALLSENSOR hall_sensor = HALLSENSOR(), IMU imu = IMU(), IMU_6DOF imu_6dof = IMU_6DOF(), IMU_9DOF imu_9dof = IMU_9DOF())
+      : hoehe_schwerpunkt(hoehe_schwerpunkt),
+        gewicht_body(gewicht_body),
+        gewicht_scheibe(gewicht_scheibe),
+        position(position),
+        inertia(inertia),
+        hall_sensor(hall_sensor),
+        imu(imu),
+        imu_6dof(imu_6dof),
+        imu_9dof(imu_9dof) {}
     //Methoden zum bewegen
     void stand_up(){
       Serial.println("stand_up");
@@ -49,10 +58,10 @@ class QUADRAT {
       Serial.println("turn_right");
       };
 };
-#endif
 
-QUADRAT::QUADRAT(double hoehe_schwerpunkt, double gewicht_body, double gewicht_scheibe, POSITION position, INERTIA inertia, HALLSENSOR hall_sensor, IMU imu, IMU_6DOF imu_6dof, IMU_9DOF imu_9dof);{
-  this->hoehe_schwerpunkt = Quadrat_hoehe_schwerpunkt;
-  this->gewicht_body = Quadrat_gewicht_body;
-  this->gewicht_scheibe = Quadrat_gewicht_scheibe;
-}
+// Definition der Klassenkonstanten
+const double QUADRAT::hoehe_schwerpunkt = 0.1;  //m über dem Boden
+const double QUADRAT::gewicht_body = 0.6;       //kg von allen Elementen außer der Scheibe
+const double QUADRAT::gewicht_scheibe = 0.1;    //kg pro Scheibe
+
+#endif
