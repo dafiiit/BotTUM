22c56b894023141de149a56c0676e35004474b4f
David@Mac <david.metzler.2003@gmail.com>
2023-05-11 18:57:05 +0200

    neuer hall test

--- tests/test_HALL/test_HALL.ino
+++ tests/test_HALL/test_HALL.ino
@@ -1,21 +1,43 @@
 
 
-const int HallSensor = 3; //Sensor pin
-//pin
-const int ledPin = 13; //LED pin
+int Led = 2;         // LED pin
+int HallSensor = 19; // Sensor pin
+volatile double time_trig;
+volatile double time_now;
+volatile float time_delta;
+float rpm_1;
+bool state = false;
 
-volatile int hallState = 0;   //Variable for reading the hall status
-void setup(){
-  pinMode(ledPin, OUTPUT);
+void IRAM_ATTR isr()
+{
+  state = !state;
+  time_delta = time_now - time_trig;
+  time_trig = time_now;
+}
+
+void setup()
+{
+  pinMode(Led, OUTPUT);
   pinMode(HallSensor, INPUT);
-  attachInterrupt(0,pin_ISR,CHANGE);
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(HallSensor), isr, RISING);
+}
+int i = 0;
+void loop()
+{
+
+  time_now = millis();
+  if (time_delta)
+  {
+    rpm_1 = 1 / (time_delta / 1000) * 60;
+    print(rpm_1);
+  }
+
+  print(time_delta);
+  delay(5);
 }
 
-void pin_ISR(){       //Interrupt Service Routine
-  hallState = digitalRead(HallSensor);
-  digitalWrite(ledPin, hallState);
-}
-
-void loop(){
-  
+void print(float delta)
+{
+  Serial.println(delta);
 }
