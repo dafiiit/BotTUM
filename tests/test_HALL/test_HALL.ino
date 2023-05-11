ff30418349e099845b22ada16918c2e7cddfbd51
David@Mac <david.metzler.2003@gmail.com>
2023-05-11 17:45:37 +0200

    Änderung von encoder Test

--- tests/test_HALL/test_HALL.ino
+++ tests/test_HALL/test_HALL.ino
@@ -1,15 +1,21 @@
+
+
 const int HallSensor = 3; //Sensor pin
-pin
-const int Led = 13; //LED pin
+//pin
+const int ledPin = 13; //LED pin
 
 volatile int hallState = 0;   //Variable for reading the hall status
 void setup(){
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(HallSensor, INPUT);
   attachInterrupt(0,pin_ISR,CHANGE);
 }
 
 void pin_ISR(){       //Interrupt Service Routine
-  hallState = digitalRead(buttonPin);
-  digitalWrite(ledPin, buttonState);
+  hallState = digitalRead(HallSensor);
+  digitalWrite(ledPin, hallState);
+}
+
+void loop(){
+  
 }
