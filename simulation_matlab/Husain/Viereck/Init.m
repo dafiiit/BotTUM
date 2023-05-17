4cef00a73c98e1a0905b92c25883844a8fc74bb0
halekri <133780391+halekri@users.noreply.github.com>
2023-05-17 17:45:08 +0200

    Add files via upload

---
+++ simulation_matlab/Husain/Viereck/Init.m
@@ -0,0 +1,6 @@
+%% Initialize 
+%Regler (control unit)
+Regler.external_force = 0.1; % [N]
+Simulation_Time = 10; %[s]
+% Bremsen (standing upright mechanism)
+%Bremsen.M_Motor = %Torque actuated by motor connected to wheel
