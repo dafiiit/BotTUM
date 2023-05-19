8933d8843fbb0895e6b8b0c9b0c25ed7b34c85ca
David@Mac <david.metzler.2003@gmail.com>
2023-05-19 20:08:32 +0200

    kleine Änderungen

--- tests/all_tests_IMU/test_IMU_6DOF_tracking/test_IMU_6DOF_tracking.ino
+++ tests/all_tests_IMU/test_IMU_6DOF_tracking/test_IMU_6DOF_tracking.ino
@@ -1,5 +1,3 @@
-//per integration sollte die Position berechnet werden
-
 // Basic demo for accelerometer readings from Adafruit MPU6050
 
 // ESP32 Guide: https://RandomNerdTutorials.com/esp32-mpu-6050-accelerometer-gyroscope-arduino/
@@ -9,130 +7,171 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include <Eigen>
 
 Adafruit_MPU6050 mpu;
+float positionX = 0.0;
+float velocityX = 0.0;
+float accelerationX = 0.0;
+float positionY = 0.0;
+float velocityY = 0.0;
+float accelerationY = 0.0;
+float positionZ = 0.0;
+float velocityZ = 0.0;
+float accelerationZ = 0.0;
+unsigned long lastUpdateTime = 0;
 
-void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-  Serial.println("Adafruit MPU6050 test!");
+void setup(void)
+{
+    Serial.begin(115200);
+    while (!Serial)
+        delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
+    Serial.println("Adafruit MPU6050 test!");
+
+    // Try to initialize!
+    if (!mpu.begin())
+    {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1)
+        {
+            delay(10);
+        }
     }
-  }
-  Serial.println("MPU6050 Found!");
+    Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange())
+    {
+    case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+    case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+    case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+    case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange())
+    {
+    case MPU6050_RANGE_250_DEG:
+        Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+        Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        Serial.println("+- 2000 deg/s");
+        break;
+    }
 
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth())
+    {
+    case MPU6050_BAND_260_HZ:
+        Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+        Serial.println("5 Hz");
+        break;
+    }
 
-  Serial.println("");
-  delay(100);
-
-    //erstelle einen Vektor für die Geschwindigkeit
-    Eigen::Vector3d Geschwindigkeit(0, 0, 0);
-    //erstelle einen Vektor für die Position
-    Eigen::Vector3d Position(0, 0, 0);
-    //erstelle einen Vektor für die Winkel
-    Eigen::Vector3d Winkel(0, 0, 0);
+    Serial.println("");
+    delay(100);
 }
+void loop()
+{
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-//diese Funktion integriert das Eingangssignal
-previousValue = 0;
-void integrate(float integrate(float previousFunctionValue, float value, float interval) {
-  return previousValue + ((value + previousFunctionValue) * interval) / 2.0;
-}
+    /* Integrate acceleration to obtain velocity */
+    unsigned long currentTime = millis();
+    float deltaTime = (currentTime - lastUpdateTime) / 1000.0; // Convert milliseconds to seconds
+    lastUpdateTime = currentTime;
 
+    float prevVelocityX = velocityX;
+    accelerationX = a.acceleration.x;
+    velocityX += (accelerationX + prevAccelerationX) * deltaTime / 2.0;
+    positionX += (velocityX + prevVelocityX) * deltaTime / 2.0;
 
-void loop() {
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+    float prevVelocityY = velocityY;
+    accelerationY = a.acceleration.y;
+    velocityY += (accelerationY + prevAccelerationY) * deltaTime / 2.0;
+    positionY += (velocityY + prevVelocityY) * deltaTime / 2.0;
 
-  
+    float prevVelocityZ = velocityZ;
+    accelerationZ = a.acceleration.z;
+    velocityZ += (accelerationZ + prevAccelerationZ) * deltaTime / 2.0;
+    positionZ += (velocityZ + prevVelocityZ) * deltaTime / 2.0;
 
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+    /* Update previous values */
+    prevAccelerationX = accelerationX;
+    prevAccelerationY = accelerationY;
+    prevAccelerationZ = accelerationZ;
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+    /* Print out the values */
+    Serial.print("Acceleration X: ");
+    Serial.print(accelerationX);
+    Serial.print(", Y: ");
+    Serial.print(accelerationY);
+    Serial.print(", Z: ");
+    Serial.print(accelerationZ);
+    Serial.println(" m/s^2");
 
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
+    Serial.print("Velocity X: ");
+    Serial.print(velocityX);
+    Serial.print(", Y: ");
+    Serial.print(velocityY);
+    Serial.print(", Z: ");
+    Serial.print(velocityZ);
+    Serial.println(" m/s");
 
-  Serial.println("");
-  delay(500);
+    Serial.print("Position X: ");
+    Serial.print(positionX);
+    Serial.print(", Y: ");
+    Serial.print(positionY);
+    Serial.print(", Z: ");
+    Serial.print(positionZ);
+    Serial.println(" m");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
+    Serial.println("");
+    delay(500);
 }
