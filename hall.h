36fe6a1fbabadde444d79713d7024b7b665415fb
David@Mac <david.metzler.2003@gmail.com>
2023-05-19 13:08:24 +0200

    var umbenennung

--- hall.h
+++ hall.h
@@ -1,55 +1,60 @@
-#include "datentypen.h"
+// #include "datentypen.h"
 
 //deklaration
 #ifndef HALLSENSOR_H
 #define HALLSENSOR_H
 class HALLSENSOR {
   private: 
-    volatile bool HallSensor_state = false;   //Variable for reading the hall status
-    int HallSensor_pin = 19;                  //beim ESP32 Interrupt pin = Physikalischer Pin
-    int HallSensor_anz_magneten = 2;
-    float HallSensor_rpm = 0;
-    float HallSensor_PhiPS = 0;
+    volatile bool state = false;    //Variable for reading the hall status
+    int pin;                        //beim ESP32 Interrupt pin = Physikalischer Pin
+    int anz_magneten;               //Anzahl der Magneten(wahrscheinlich 2)
+    float rpm = 0;
+    float phi_punkt_scheibe = 0;
     volatile double time_trig = 0;
     volatile double time_now = 0;
     volatile double time_delta = 0;
-    int HallSensor_Abtastrate = 115200;
+    int abtastrate;        //Abtastrate für die serielle Kommunikation
   
   public:
     HALLSENSOR();
     void hall_setup();
-    float get_hall_PhiPS();  
-    float get_hall_RPM();     
+    float get_hall_phi_punkt_scheibe();  
+    float get_hall_rpm();     
   private:
-    static void hall_ISR();
+    static void hall_isr();
 };
 #endif
 
-HALLSENSOR::HALLSENSOR() {}
+//Konstruktormethode, in welcher die Attribute anz_magneten, abtastrate und pin gesetzt werden
+HALLSENSOR::HALLSENSOR(
+  int anz_magneten = 2, 
+  int abtastrate = 115200, 
+  int pin
+  ) : anz_magneten(anz_magneten), abtastrate(abtastrate), pin(pin) {}
 
 void HALLSENSOR::hall_setup(){
-  pinMode(HallSensor_pin, INPUT);
-  Serial.begin(HallSensor_Abtastrate);
-  attachInterrupt(digitalPinToInterrupt(HallSensor_pin), hall_ISR, RISING); 
+  pinMode(self.pin, INPUT);
+  Serial.begin(self.abtastrate);
+  attachInterrupt(digitalPinToInterrupt(self.pin), hall_ISR, RISING); 
 }
-void HALLSENSOR::hall_ISR(){       
-  HallSensor_state = !HallSensor_state;
+void HALLSENSOR::hall_isr(){       
+  self.state = !self.state;
   time_delta = time_now - time_trig;
   time_trig = time_now;
 }
-float HALLSENSOR::get_hall_PhiPS(){
+float HALLSENSOR::get_hall_phi_punkt_scheibe(){
   time_now = millis();
   if (time_delta)
   {
-    HallSensor_PhiPS = (1 /(time_delta / 1000))/HallSensor_anz_magneten;
-    return HallSensor_PhiPS;
+    self.phi_punkt_scheibe = (1 /(time_delta / 1000))/self.anz_magneten;
+    return self.phi_punkt_scheibe;
   }
 }
-float HALLSENSOR::get_hall_RPM(){
+float HALLSENSOR::get_hall_rpm(){
   time_now = millis();
   if (time_delta)
   {
-    HallSensor_rpm = (1 /( (time_delta / 1000) * 60) )/HallSensor_anz_magneten;
-    return HallSensor_rpm;
+    self.rpm = (1 /( (time_delta / 1000) * 60) )/self.anz_magneten;
+    return self.rpm;
   }
 }
--- main.ino
+++ main.ino
@@ -5,7 +5,7 @@
 #include <tgmath.h>
 #include <Serial.h>
 
-#include "datentypen.h"
+//#include "datentypen.h"
 #include "hall.h"
 //#include "imu.h"
 //#include "quadrat.h"
@@ -13,15 +13,15 @@
 HALLSENSOR hallSensor;   // Instanziierung der HALLSENSOR-Klasse
 
 void setup(){
-  Serial.begin(hallSensor.HallSensor_Abtastrate);
+  Serial.begin(hallSensor.abtastrate);
   hallSensor.hall_setup();   // Initialisierung des Hallsensors
   //IMU.imu_setup()
 }
 
 void loop() {
 
-  float rpm = hallSensor.get_hall_RPM();      // RPM abrufen
-  float phiPS = hallSensor.get_hall_PhiPS();  // PhiPS abrufen
+  float rpm = hallSensor.get_hall_rpm();      // RPM abrufen
+  float phiPS = hallSensor.get_hall_phi_punkt_scheibe();  // PhiPS abrufen
 
   Serial.print("PhiPS: ");
   Serial.println(phiPS);
