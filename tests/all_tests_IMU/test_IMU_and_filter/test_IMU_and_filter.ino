f726f272d6ee9fe88d60c65b1f0b20f7fff45a4e
David@Mac <david.metzler.2003@gmail.com>
2023-05-27 22:21:16 +0200

    imu mit filter code hinzugefügt

---
+++ tests/all_tests_IMU/test_IMU_and_filter/test_IMU_and_filter.ino
@@ -0,0 +1,106 @@
+//dies ist der Code von Zlatina
+#include <Wire.h>
+
+const int MPU = 0x68; // MPU6050 I2C address
+float AccX, AccY, AccZ;
+float GyroX, GyroY, GyroZ;
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+float roll, pitch, yaw;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+
+// Gyroscope bias estimation variables
+const int numSamples = 200;
+float gyroBiasX = 0.0, gyroBiasY = 0.0, gyroBiasZ = 0.0;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission(true);
+
+  // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);
+  Wire.write(0x10);
+  Wire.endTransmission(true);
+
+  // Configure Gyro Sensitivity - Full Scale Range (default +/- 250deg/s)
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1B);
+  Wire.write(0x10);
+  Wire.endTransmission(true);
+  delay(20);
+
+  // Perform gyro bias estimation
+  for (int i = 0; i < numSamples; i++) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    GyroX = (Wire.read() << 8 | Wire.read()) / 131;
+    GyroY = (Wire.read() << 8 | Wire.read()) / 131;
+    GyroZ = (Wire.read() << 8 | Wire.read()) / 131;
+
+    gyroBiasX += GyroX;
+    gyroBiasY += GyroY;
+    gyroBiasZ += GyroZ;
+
+    delay(3); // Add a small delay between samples
+  }
+
+  gyroBiasX /= numSamples;
+  gyroBiasY /= numSamples;
+  gyroBiasZ /= numSamples;
+} 
+void loop() {
+  previousTime = currentTime;
+  currentTime = millis();
+  elapsedTime = (currentTime - previousTime) / 1000.0;
+
+  // Read accelerometer data
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+
+  AccX = (Wire.read() << 8 | Wire.read());
+  AccY = (Wire.read() << 8 | Wire.read());
+  AccZ = (Wire.read() << 8 | Wire.read());
+
+  // Convert accelerometer values to degrees
+  accAngleX = atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180.0 / PI;
+  accAngleY = atan(-AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180.0 / PI;
+
+  // Read gyroscope data
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+  GyroX = (Wire.read() << 8 | Wire.read()) / 131;
+  GyroY = (Wire.read() << 8 | Wire.read()) / 131;
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131;
+
+  // Apply gyro bias compensation
+  GyroX -= gyroBiasX;
+  GyroY -= gyroBiasY;
+  GyroZ -= gyroBiasZ;
+
+  // Update gyro angles
+  gyroAngleX += GyroX * elapsedTime;
+  gyroAngleY += GyroY * elapsedTime;
+  yaw += GyroZ * elapsedTime;
+
+  // Complementary filter - combine accelerometer and gyro angles
+  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
+  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+
+  Serial.print("Roll: ");
+  Serial.print(roll);
+  Serial.print("   Pitch: ");
+  Serial.print(pitch);
+  Serial.print("   Yaw: ");
+  Serial.println(yaw);
+}
---
+++ tests/tests_all_NodeRed/test_mqtt_imu_wifi/test_WiFi_and_MQTT.h
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "secure.h"
+//#include "test_mqtt_imu_wifi.h"
+
+//funktioniert
+
+const char* mqttServer = "test.mosquitto.org";
+const int mqttPort = 1883;
+const char* mqttTopic = "RoboTUM/steuerung";
+const char* mqttClientId = "ESP32Client";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void mqtt_setup() {
+
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // ab hier hat der Rest nichts mehr mit Wlan sondern nur noch mit MQTT zu tun
+
+  // Set up MQTT server and callback function
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+
+  // Connect to MQTT broker
+  mqtt_reconnect();
+}
+
+void mqtt_loop() {
+  if (!client.connected()) {
+    mqtt_reconnect();
+  }
+  client.loop();
+
+  // Publish a test message
+  //String message = "vorwärts";
+  //client.publish(mqttTopic, message.c_str());
+  // client.callback("RoboTUM/steuerung")
+  //Serial.println("Message published");
+
+  //delay(5000);  // Wait 5 seconds before publishing the next message
+
+}
+
+void mqtt_reconnect() {
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT broker...");
+    if (client.connect(mqttClientId)) {
+      Serial.println("Connected to MQTT broker");
+      client.subscribe(mqttTopic);  // Subscribe to the topic (optional)
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" Retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Handle incoming messages
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.print("Received message: ");
+  Serial.println(message);
+}
